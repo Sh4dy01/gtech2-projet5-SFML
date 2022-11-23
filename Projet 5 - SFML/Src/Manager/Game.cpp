e66a5f5ac31e202a5d36c69cf3d2c35e70da80e4
guilian pipart <guilian.pipart@gmail.com>
2022-11-23 11:47:46 +0100

    creating map class

--- Projet 5 - SFML/Src/Manager/Game.cpp
+++ Projet 5 - SFML/Src/Manager/Game.cpp
@@ -1,5 +1,12 @@
 #include "Game.h"
+#include "Map/Map.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
+using namespace std;
+static const char* const SAVEMAP_FILE = "Maps.txt";
 
 sf::RenderWindow* Game::window;
 int Game::width;
@@ -23,4 +30,60 @@ void Game::Initialization()
 	stateCombat   = new StateCombat();
 
 	State::setDefaultState(stateMainMenu);
+}
+
+
+void Game::MapLoader()
+{
+	ifstream f(SAVEMAP_FILE);
+	bool mapLoading = false;
+	bool tabLoading = false;
+	int width = 0;
+	int length = 0;
+	int indexX = 0;
+	int indexY = 0;
+
+	// If settings file does not exist yet, return error.
+	if (!f) {
+		cout << "error : file Maps.txt does not exist." << endl;
+		return;
+	}
+
+	string line, id;
+	while (!f.eof())
+	{
+		// Get line.
+		getline(f, line);
+
+		// Ignore empty line.
+		if (line.empty()) {
+			continue;
+		}
+
+		// Formatted reading.
+		stringstream ss(line);
+		ss >> id;
+
+		if (id == ">") {
+			mapLoading = true;
+		}
+
+		if (mapLoading) {
+
+			if (id == "length") {
+				ss >> length;
+			}
+			else if (id == "width") {
+				ss >> width;
+			}
+
+
+
+		}
+
+		// create tab if length and width aren't empty.
+
+
+		f.close();
+	}
 }
