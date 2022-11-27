7c65d7c8cf7c000920ea4276383298645a6d0d3a
Hugo.M <h.maestracci83@gmail.com>
2022-11-27 16:21:53 +0100

    quick

--- Projet 5 - SFML/Src/Manager/Audio/SoundManager.cpp
+++ Projet 5 - SFML/Src/Manager/Audio/SoundManager.cpp
@@ -2,8 +2,16 @@
 
 #include <iostream>
 
+SoundManager::SoundManager()
+{
+}
+
 void SoundManager::LoadSound(std::string name)
 {
 	if (!buffer->loadFromFile(BASE_SOUND_PATH + name + BASE_SOUND_EXTENSION))
 		std::cout << name << " : sound file doesn't exist" << std::endl;
+}
+
+void SoundManager::PlaySound()
+{
 }
